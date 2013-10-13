8144c22f11ae502cf351b4187b2e9aec92269217
Brian Flad <bflad417@gmail.com>
2013-10-13 18:39:14 -0400

    Minitest fixups

--- files/default/tests/minitest/package_test.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('../support/helpers', __FILE__)
-
-describe_recipe "gflags::package" do
-  include Helpers::Gflags
-
-  # Other package_*_test will check their packages
-end
